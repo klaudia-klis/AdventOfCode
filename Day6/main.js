de8b430364f2b5ac3090b88d8fe0b201c5083f59
Klaudia <kklis@interia.pl>
2022-12-14 14:51:55 +0100

    Day 6 - part 2

--- Day6/main.js
+++ Day6/main.js
@@ -1,7 +1,9 @@
 const {readFileSync, promises: fsPromises} = require('fs');
 
 function syncReadFile(filename) {
-  const contents = readFileSync(filename, 'utf-8');
+  const contents = readFileSync(filename, 'utf-8')
+    .replace(/\r/g, "")
+    .trim();
   const arr = [contents][0].split('');
   
   function findMarker(arr) {
@@ -13,7 +15,19 @@ function syncReadFile(filename) {
     return result.length+4;
   }
   
-  console.log(findMarker(arr));
+  function part2(arr) {
+    let result;
+    for (let i = 0; i < arr.length; i++) {
+      const unique_signal = new Set([...arr.slice(i,i+14)]);
+      
+      if (unique_signal.size === 14) {
+        result = i + 14;
+        break;
+      }
+    }
+    return result;
+  }
+  console.log(part2(arr));
 }
 
 syncReadFile('/Users/klaudiaklis/Sites/AdventOfCode/Day6/day6data.txt');
