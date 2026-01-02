1ed0c36ae8124e0ecb70c5b28ab4f92ad1e7d599
Klaudia <kklis@interia.pl>
2026-01-02 12:39:38 +0100

    Completed day 3 part 2

--- 2025/Day3/main.js
+++ 2025/Day3/main.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs';
 function syncReadFile(filename) {
   const contents = readFileSync(filename, 'utf-8');
 
-  return f1(contents);
+  return f2(contents);
 }
 
 export function f1(input) {
@@ -47,6 +47,46 @@ export function findLargestJoltage(input) {
     }
 
     return Number([largestNumber, secondLargest].join(''));
+}
+
+export function findLargestJoltagePart2(input) {
+    let length = input.length;
+    let num = 11;
+
+    const array = input.split('').map(a => Number(a));
+    let slicedArray = array.slice(0, length - 11);
+    let largestNumber = Math.max(...slicedArray);
+    let index = array.indexOf(largestNumber);
+    let result = [];
+    num--;
+
+    result.push(largestNumber);
+    while (num >= 0) {
+        slicedArray = array.slice(index + 1, length - num);
+        largestNumber = Math.max(...slicedArray);
+        index = index + 1 + slicedArray.indexOf(largestNumber);
+
+        num--;
+        result.push(largestNumber);
+    }
+    
+    return Number(result.join(''));
+}
+
+export function f2(input) {
+    const banks = toBanks(input);
+    let result = [];
+    
+    for (let i = 0; i < banks.length; i++) {
+        result.push(findLargestJoltagePart2(banks[i]));
+    }
+
+    let sum = 0;
+    for (let i = 0; i < result.length; i++) {
+        sum += result[i];
+    }
+    
+    return sum;
 }
 
 console.log(syncReadFile('/Users/klaudiaklis/Rzeczy/AdventOfCode/2025/Day3/input.txt'));
--- 2025/Day3/test.mjs
+++ 2025/Day3/test.mjs
@@ -1,6 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import { f1, toBanks, findLargestJoltage } from './main.js';
+import { f1, f2, toBanks, findLargestJoltage, findLargestJoltagePart2 } from './main.js';
 
 const testInput = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
 const testBanks = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"];
@@ -9,11 +9,22 @@ test("f1", () => {
   assert.strictEqual(f1(testInput), 357);
 });
 
+test("f2", () => {
+  assert.strictEqual(f2(testInput), 3121910778619);
+});
+
 test("findLargestJoltage", () => {
-  assert.strictEqual(findLargestJoltage(987654321111111), 98);
-  assert.strictEqual(findLargestJoltage(811111111111119), 89);
-  assert.strictEqual(findLargestJoltage(234234234234278), 78);
-  assert.strictEqual(findLargestJoltage(818181911112111), 92);
+  assert.strictEqual(findLargestJoltage('987654321111111'), 98);
+  assert.strictEqual(findLargestJoltage('811111111111119'), 89);
+  assert.strictEqual(findLargestJoltage('234234234234278'), 78);
+  assert.strictEqual(findLargestJoltage('818181911112111'), 92);
+});
+
+test("findLargestJoltagePart2", () => {
+  assert.strictEqual(findLargestJoltagePart2('987654321111111'), 987654321111);
+  assert.strictEqual(findLargestJoltagePart2('811111111111119'), 811111111119);
+  assert.strictEqual(findLargestJoltagePart2('234234234234278'), 434234234278);
+  assert.strictEqual(findLargestJoltagePart2('818181911112111'), 888911112111);
 });
 
 test("toBanks", () => {
